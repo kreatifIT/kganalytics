f0d167781f5396182e0551e89423b780fe1733e9
Alex Platter <alex.platter@gmail.com>
2021-11-10 10:54:52 +0100

    rex.kga existance check added in js

--- assets/js/kganayltics.js
+++ assets/js/kganayltics.js
@@ -2,14 +2,16 @@ var Kganayltics = (function ($) {
     var _this = {functions: {}};
 
     _this.functions.setClientId = function (clientId) {
-        if (clientId != rex.kga.clientId) {
-            if (rex.kga.debug) {
-                console.debug('Sending clientId to kga_api');
+        if (typeof rex.kga != 'undefined') {
+            if (clientId != rex.kga.clientId) {
+                if (rex.kga.debug) {
+                    console.debug('Sending clientId to kga_api');
+                }
+                $.post(rex.kga.apiUrls.setClientId, {clientId: clientId});
             }
-            $.post(rex.kga.apiUrls.setClientId, {clientId: clientId});
-        }
-        if (rex.kga.debug) {
-            console.debug('GA4.clientId = ' + rex.kga.clientId);
+            if (rex.kga.debug) {
+                console.debug('GA4.clientId = ' + rex.kga.clientId);
+            }
         }
     };
 
