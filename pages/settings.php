e800d59f6922a5d7fe1fbc76f51b2c28b3f515e2
Michael Rainer <m.rainer@kreatif.it>
2022-03-23 16:27:51 +0100

    queue min wait time message improved

--- pages/settings.php
+++ pages/settings.php
@@ -36,7 +36,7 @@ $select->addOptions([1 => 'Server', 0 => 'Client (JS) -> benötigt Tag Manager']
 
 $field = $form->addTextField('queue_min_wait_time', null, ["class" => "form-control"]);
 $field->setLabel('Queue Min Wait Time');
-$field->setNotice( 'Mindest-Dauer, bis die serverseitigen Events gesendet werden.');
+$field->setNotice( 'Mindest-Dauer in Sekunden, bis die serverseitigen Events gesendet werden. (Standard = 300s)');
 
 $field = $form->addTextField('property_id', null, ["class" => "form-control"]);
 $field->setLabel('V4 Property-ID');
